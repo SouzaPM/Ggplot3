aad4addbd7b6939e2aeaf8c0ab8297ae5aa7a854
SouzaPM <mpsouza@calpoly.edu>
2026-05-29 15:46:48 -0700

    Add files via upload
    Created the new .R files for the project step along with the testing spaces, wrote descriptions for each function we are going to define.

---
+++ ggplot3/R/Ranking.R
@@ -0,0 +1,1 @@
+#' creates a function to rank all the coutnries by certain defined variables
---
+++ ggplot3/R/all_ranks.R
@@ -0,0 +1,1 @@
+#' We are going to create an interactive table of all (usually countries) ranks, calling the ranks function
